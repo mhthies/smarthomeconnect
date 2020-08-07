ccb6ee61bec150eab0d5e1807249c0c4421e0e88
Michael Thies <mail@mhthies.de>
2020-08-07 17:48:36 +0200

    datatypes: Fix RangeFloat1 type and type conversions

--- shc/datatypes.py
+++ shc/datatypes.py
@@ -9,7 +9,7 @@ logger = logging.getLogger(__name__)
 class RangeFloat1(float):
     def __new__(cls, *args, **kwargs):
         # noinspection PyArgumentList
-        res = int.__new__(cls, *args, **kwargs)
+        res = float.__new__(cls, *args, **kwargs)
         if not 0.0 <= res <= 1.0:
             raise ValueError("{} is out of the allowed range for type {}".format(res, cls.__name__))
         return res
@@ -28,6 +28,8 @@ register_converter(RangeUInt8, RangeInt0To100,
                    lambda v: RangeInt0To100(round(v * 100 / 255)))
 register_converter(RangeUInt8, RangeFloat1, lambda v: RangeFloat1(v / 255))
 register_converter(RangeInt0To100, RangeFloat1, lambda v: RangeFloat1(v / 100))
+register_converter(RangeInt0To100, RangeUInt8, lambda v: RangeUInt8(round(v / 100 * 255)))
+register_converter(RangeFloat1, RangeUInt8, lambda v: RangeUInt8(round(v * 255)))
 register_converter(RangeUInt8, bool, lambda v: bool(v))
 register_converter(int, RangeUInt8, lambda v: RangeUInt8(v))
 register_converter(RangeFloat1, bool, lambda v: bool(v))
