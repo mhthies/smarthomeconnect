6198af6fe009e773095a3b5b7fc2d6ac534f4952
Michael Thies <mail@mhthies.de>
2020-08-14 19:12:28 +0200

    base: Add some documentation

--- shc/base.py
+++ shc/base.py
@@ -134,7 +134,22 @@ class Subscribable(Connectable[T], Generic[T], metaclass=abc.ABCMeta):
               for target in self._triggers)
         )
 
-    def subscribe(self, subscriber: Writable[S], convert: Union[Callable[[T], S], bool] = False):
+    def subscribe(self, subscriber: Writable[S], convert: Union[Callable[[T], S], bool] = False) -> None:
+        """
+        Subscribe a writable object to this object to be updated, when this object publishes a new value.
+
+        The subscriber's :meth:`Writable.write` method will be called for any new value published by this object, as
+        long as the subscriber did not lead to the relevant update of this object (i.e. is not included in the
+        `source` list). The source list passed to the subscriber's `write` method will contain this object as the last
+        entry.
+
+        :param subscriber: The object to subscribe for updates
+        :param convert: A callable to convert this object's new value to the data `type` of the subscriber or `True` to
+            choose the appropriate conversion function automatically.
+        :raises TypeError: If the `type` of the subscriber does not match this object's type and `convert` is False *or*
+            if `convert` is True but no type conversion is known to convert this object's type into the subscriber's
+            type.
+        """
         converter: Optional[Callable[[T], S]]
         if callable(convert):
             converter = convert
@@ -148,6 +163,43 @@ class Subscribable(Connectable[T], Generic[T], metaclass=abc.ABCMeta):
         self._subscribers.append((subscriber, converter))
 
     def trigger(self, target: LogicHandler) -> LogicHandler:
+        """
+        Register a logic handler function to be triggered when this object is updated.
+
+        This method can be used as a decorator for custom logic handler functions. Alternatively, it can simply called
+        with a function object: `some_subscribable.trigger(some_handler_function).
+
+        The `target` function must be an async coroutine that takes two arguments: The new value of this object and the
+        source/trace of the event (a list of objects that led to the handler being tiggered). The handler function
+        must make sure to prevent infinite recursive feedback loops: In contrast to subscribed objects, logic handler
+        functions are also triggered, if they led to the object being updated (i.e. they are already conained in the
+        `source` list). Thus, they should skip execution if called recursively. It should also append itself to the
+        `source` list and pass the extended list to all :meth:`Writable.write` calls it does.
+
+        To ensure all this for a custom handler function, use the :func:`handler` decorator::
+
+            @some_subscribable.trigger
+            @handler()
+            async def some_handler_function(value, source):
+                ...
+                some_writable.write(value + 1)
+                ...
+
+        You may even use multiple trigger decorators::
+
+            @some_subscribable.trigger
+            @another_subscribable.trigger
+            @handler()
+            async def some_handler_function(value, source):
+                if source[-1] is some_subscribable:
+                    ...
+                else:
+                    ...
+
+        :param target: The handler function/coroutine to be triggered on updates. Must comply with the requirements
+            mentioned above.
+        :return: The `target` function (unchanged)
+        """
         self._triggers.append(target)
         return target
 
@@ -173,6 +225,13 @@ class Reading(Connectable[T], Generic[T], metaclass=abc.ABCMeta):
         self._default_provider = (provider, converter)
 
     async def _from_provider(self) -> Optional[T]:
+        """
+        :meth:`Readable.read`() the current value from the default provider and convert it to this object's type, if
+        necessary, using the registered converter function.
+
+        :return: The default provider's current value *or* `None` if no default provider is set or it's `read` method
+            raises an UninitializedError.
+        """
         if self._default_provider is None:
             return None
         provider, convert = self._default_provider
@@ -184,6 +243,24 @@ class Reading(Connectable[T], Generic[T], metaclass=abc.ABCMeta):
 
 
 def handler(reset_source=False, allow_recursion=False) -> Callable[[LogicHandler], LogicHandler]:
+    """
+    Decorator for custom logic handler functions.
+
+    Wraps a custom logic handler functions to make sure it is suited to be registered to be triggered by a subscribable
+    object with :meth:`Subscribable.trigger`. It makes sure that
+    * exceptions, occuring during execution, are logged,
+    * the `source` is extended with the logic handler itself and magically passed to all :meth:`Writable.write` calls
+    * the `source` can magically be passed when called directly by other logic handlers
+    * the execution is skipped when called recursively (i.e. the logic handler is already contained in the `source` list
+
+    :param reset_source: If True, the source which is magically passed to all `write` calls, only contains the logic
+        handler itself, not the previous `source` list, which led to the handler's execution. This can be used to
+        change an object's value, which triggers this logic handler. This may cause infinite recursive feedback loops,
+        so use with care!
+    :param allow_recursion: If True, recursive execution of the handler is not skipped. The handler must check the
+        passed values and/or the `source` list itself to prevent infinite feedback loops via `write` calls or calls to
+        other logic handlers – especiaally when used together with `reset_source`.
+    """
     def decorator(f: LogicHandler) -> LogicHandler:
         @functools.wraps(f)
         async def wrapper(value, source: Optional[List[Any]] = None) -> None:
