f2b873065e597c5ec7faeee225040a7a876ca434
Michael Thies <mail@mhthies.de>
2020-10-04 13:09:26 +0200

    web: Fix ImageMap widget sub items popup position (incl. z layer)

--- shc/web/static/main.js
+++ shc/web/static/main.js
@@ -343,7 +343,12 @@ const WIDGET_TYPES = new Map([
 $(function() {
     $('.main-menu .ui.dropdown').dropdown();
     $('.ui.sidebar').sidebar({transition: 'overlay'}).sidebar('attach events', '#mobile_item');
-    $('.shc.image-container .with-popup').each(function(){
-        $(this).popup({on: 'click'});
+    $('.shc.image-container .with-popup>*').each(function(){
+        $(this).popup({
+            on: 'click',
+            popup: $(this).parent().attr('data-popup-id'),
+            position: $(this).parent().attr('data-popup-position'),
+            movePopup: false,
+            forcePosition: true});
     });
 });
