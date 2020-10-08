7d2edadb055ec6feec580a5bf1e93f3e78ddebd1
Michael Thies <mail@mhthies.de>
2020-10-08 15:49:13 +0200

    Add basic test for example

---
+++ test/test_examples.py
@@ -0,0 +1,17 @@
+import asyncio
+import unittest
+
+import shc
+
+
+class TestUiShowcase(unittest.TestCase):
+    def test_basic(self) -> None:
+        # Automatically shutdown after one second
+        async def shutdown() -> None:
+            await asyncio.sleep(1)
+            await shc.supervisor.stop()
+        shc.supervisor.event_loop.create_task(shutdown())
+
+        import example.ui_showcase
+        shc.main()
+
