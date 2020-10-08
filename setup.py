39e42a20d1f3d0c6bbde2a2117a82e7301fd9c9e
Michael Thies <mail@mhthies.de>
2020-10-08 21:41:35 +0200

    Add README as long_description in package metadata

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
+import os.path
 from setuptools import setup, find_packages
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='smarthomeconnect',
     version='0.1',
     description='The Smart Home Connect home automation framework based on AsyncIO',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Michael Thies',
     author_email='mail@mhthies.de',
     url='https://github.com/mhthies/smarthomeconnect',
