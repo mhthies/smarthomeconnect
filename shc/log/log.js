0070a2e5cb590be665871ecd5b24dec4a5f513d1
Michael Thies <mail@mhthies.de>
2020-11-07 22:16:31 +0100

    log: Fix LogListWidget update logic

--- shc/log/log.js
+++ shc/log/log.js
@@ -2,13 +2,74 @@
 
 function LogListWidget(domElement, _writeValue) {
     const id = parseInt(domElement.getAttribute('data-id'));
+    const interval = parseInt(domElement.getAttribute('data-interval')); // in milliseconds
+    const dateTimeFormat = new Intl.DateTimeFormat(undefined, {
+        month: 'numeric', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric'});
     this.subscribeIds = [id];
 
+    let lastRow = null;
+
+    /// Called regularly to remove old entries (older than `interval` milliseconds`)
+    function cleanUp() {
+        let now = new Date();
+        let timeout = new Date(now.getTime() - interval);
+        let children_reversed = Array.from(domElement.childNodes);
+        children_reversed.reverse();
+        for (let child of children_reversed) {
+            if (child.logTimeStamp < timeout) {
+                child.remove();
+            } else {
+                // rows are ordered, so we can break when finding the first one still in range.
+                break;
+            }
+        }
+    }
+    setInterval(cleanUp, 5000);
+
+    function addRow(timestamp, value) {
+        // Create row
+        let node = document.createElement("div");
+        node.logTimeStamp = timestamp;
+        domElement.prepend(node);
+
+        // Add formatted timestamp
+        node.appendChild(document.createTextNode(dateTimeFormat.format(timestamp)));
+
+        // Add value box
+        let value_box = document.createElement("div");
+        value_box.classList.add('right');
+        value_box.classList.add('floated');
+        value_box.innerText = value;
+        node.appendChild(value_box);
+
+        return node;
+    }
+
     this.update = function(value, for_id) {
-        for (let row of value) {
-            let node = document.createElement("div");
-            node.appendChild(document.createTextNode(row[0] + "  --  " + row[1]));
-            domElement.appendChild(node);
+
+        // Full initialization after (re)connect
+        if (value['init']) {
+            domElement.innerHTML = '';
+            for (let row of value['data']) {
+                // parse the timestamp
+                let timestamp = Date.parse(row[0]);
+                lastRow = addRow(timestamp, row[1]);
+            }
+
+        // Incremental update
+        } else {
+            for (let row of value['data']) {
+                let timestamp = Date.parse(row[0]);
+                // If timestamp is equal to the last record's timestamp, update that value
+                if (lastRow && timestamp === lastRow.logTimeStamp) {
+                    const last_value_box = lastRow.getElementsByTagName('div')[0];
+                    last_value_box.innerText = row[1];
+
+                // If the timestamp is newer, add a new row. If the row is older than the last row, we ignore it.
+                } else if (!(lastRow && timestamp < lastRow.logTimeStamp)) {
+                    lastRow = addRow(timestamp, row[1]);
+                }
+            }
         }
     };
 }
--- shc/log/widgets.py
+++ shc/log/widgets.py
@@ -11,12 +11,15 @@ class LogListWidget(WebPageItem):
         # TODO add formatting
         # TODO allow multiple variables
         self.connector = LoggingWebUIView(variable, interval)
+        self.interval = interval
 
     def register_with_server(self, page: WebPage, server: WebServer) -> None:
         server.add_js_file(Path(__file__).parent / 'log.js')
 
     async def render(self) -> str:
-        return '<div data-widget="log.log_list" data-id="{}"></div>'.format(id(self.connector))
+        return '<div data-widget="log.log_list" data-id="{}" data-interval="{}" ' \
+               'style="max-height: 300px; overflow-y: auto;"></div>'\
+            .format(id(self.connector), round(self.interval.total_seconds() * 1000))
 
     def get_connectors(self) -> Iterable[WebUIConnector]:
         return [self.connector]
