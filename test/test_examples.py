f20a210d4423fdfbe4c95f2820d46ae37a8635b7
Michael Thies <mail@mhthies.de>
2020-11-14 15:23:43 +0100

    test: Refactor basic example tests, add test for server client example

--- test/test_examples.py
+++ test/test_examples.py
@@ -4,25 +4,32 @@ import unittest
 import shc
 
 
-class TestUiShowcase(unittest.TestCase):
-    def test_basic(self) -> None:
-        # Automatically shutdown after one second
-        async def shutdown() -> None:
-            await asyncio.sleep(1)
-            await shc.supervisor.stop()
-        shc.supervisor.event_loop.create_task(shutdown())
+# Helper coroutine to trigger shutdown after one second automatically
+async def shutdown() -> None:
+    await asyncio.sleep(1)
+    await shc.supervisor.stop()
 
+
+class BasicTest(unittest.TestCase):
+    def test_ui_showcase(self) -> None:
+        shc.supervisor.event_loop.create_task(shutdown())
         import example.ui_showcase  # type: ignore
         shc.main()
 
-
-class TestUiLoggingShowcase(unittest.TestCase):
-    def test_basic(self) -> None:
-        # Automatically shutdown after one second
-        async def shutdown() -> None:
-            await asyncio.sleep(1)
-            await shc.supervisor.stop()
+    def test_ui_logging_showcase(self) -> None:
         shc.supervisor.event_loop.create_task(shutdown())
 
         import example.ui_logging_showcase  # type: ignore
         shc.main()
+
+    def test_server_client_example(self) -> None:
+        shc.supervisor.event_loop.create_task(shutdown())
+
+        # The examples should actually be able to coexist in a single SHC instance
+        import example.server_client.server  # type: ignore
+        import example.server_client.client  # type: ignore
+        shc.main()
+
+    def tearDown(self) -> None:
+        shc.supervisor._REGISTERED_INTERFACES.clear()
+        shc.timer.timer_supervisor.supervised_timers.clear()
