00dcb499571d6054544139d5519a903cd78cbefc
Michael Thies <mail@mhthies.de>
2021-01-02 14:26:40 +0100

    interface._helper: Improve docs

--- shc/interfaces/_helper.py
+++ shc/interfaces/_helper.py
@@ -9,8 +9,29 @@ logger = logging.getLogger(__name__)
 
 
 class SupervisedClientInterface(AbstractInterface, metaclass=abc.ABCMeta):
+    """
+    Abstract base class for client interfaces, providing run task supervision and automatic reconnects
+
+    This class can be used as a base class for client interface implementations to simplify error handling and automatic
+    reconnection attempts. The core of its functionality is the `_supervise` Task. It is started at interface `start()`
+    and runs until shutdown of the interface. It starts and supervises the implementation-specific :meth:`_run` Task,
+    which typically contains a loop for handling incoming messages. In addition, it calls :meth:`_connect` **before**
+    and :meth:`_subscribe` **after** starting the run task. If the run task exits unexpectedly and
+    :attr:`auto_reconnect` is enabled, reconnecting the interface via `_connect`, `_run` and `_subscribe` is attempted.
+    For shutting down the interface (and stopping the run task in case of a subscribe error), `_disconnect` must be
+    implemented in such a way, that it shuts down the run task.
+    """
     def __init__(self, auto_reconnect: bool = True, failsafe_start: bool = False):
         super().__init__()
+        """
+        :param auto_reconnect: If True (default), the supervisor tries to reconnect the interface automatically with
+            exponential backoff (`backoff_base` * `backoff_exponent` ^ n seconds sleep), when `_run` exits unexpectedly
+            lost. Otherwise, the complete SHC system is shut down on connection errors.
+        :param failsafe_start: If True and auto_reconnect is True, the interface allows SHC to start up, even if the
+            `_connect` or `_subscribe` fails in the first try. The connection is retried in background with exponential
+            backoff (see `auto_reconnect` option). Otherwise (default), the first connection attempt on startup is not
+            retried and will raise an exception from `start()` on failure, even if `auto_reconnect` is True.
+        """
         self.auto_reconnect = auto_reconnect
         self.failsafe_start = failsafe_start and auto_reconnect
         self.backoff_base = 1  #: First wait interval for exponential backoff in seconds
@@ -44,12 +65,14 @@ class SupervisedClientInterface(AbstractInterface, metaclass=abc.ABCMeta):
         Entrypoint for the run task, which handles messages etc. while the connection is active.
 
         This coroutine is started in a separate task *after* :meth:`_connect` has completed sucessfully. As soon as it
-        is ready, it should set the :attr:`_running` event. Only then the :meth:`_subscribe` method is called and the
+        is ready, it must set the :attr:`_running` event. Only then the :meth:`_subscribe` method is called and the
         startup of the interface is reported as finished.
 
         The `_run` coroutine should be stoppable by calling :meth:`_disconnect` (i.e. it should return or raise an
-        exception when `_disconnect` is called). In addition, it should return or raise an exception when a client error
-        occurs, in order to trigger a reconnect attempt.
+        exception when `_disconnect` is called). If in doubt, just add a new :class:`asyncio.Event` and use
+        :func:`asyncio.wait` with the event's `.wait()` method and your original future in all places, where you need to
+        await a future. In addition, `_run` should return or raise an exception when a client error occurs, in order to
+        trigger a reconnect attempt.
         """
         pass
 
@@ -78,7 +101,7 @@ class SupervisedClientInterface(AbstractInterface, metaclass=abc.ABCMeta):
         called multiple times without reconnect. This method should not raise Exceptions but instead try its best to
         shut down the interface.
 
-        Calling this coroutine should somehow stop the run task. I.e. :meth:`_run` should return or raise an exception
+        Calling this coroutine must somehow stop the run task. I.e. :meth:`_run` should return or raise an exception
         shortly afterwards.
         """
         pass
@@ -119,7 +142,7 @@ class SupervisedClientInterface(AbstractInterface, metaclass=abc.ABCMeta):
                 done, _ = await asyncio.wait((wait_running, run_task), return_when=asyncio.FIRST_COMPLETED)
                 if wait_running not in done:
                     wait_running.cancel()
-                    if run_task not in done:
+                    if run_task not in done:  # This should not happen (without timeout)
                         await self._disconnect()
                         await run_task
                     raise RuntimeError("Run task stopped before _running has been set")
@@ -130,7 +153,7 @@ class SupervisedClientInterface(AbstractInterface, metaclass=abc.ABCMeta):
                 # TODO timeout
                 done, _ = await asyncio.wait((subscribe_task, run_task), return_when=asyncio.FIRST_COMPLETED)
                 if subscribe_task not in done:
-                    if run_task not in done:
+                    if run_task not in done:  # This should not happen (without timeout)
                         await self._disconnect()
                         await run_task
                     raise RuntimeError("Run task stopped before _subscribe task finished")
