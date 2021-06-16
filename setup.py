cc7e9f3d25f87898b4291c4e0a742ce4a8e4174b
Michael Thies <mail@mhthies.de>
2021-06-16 20:53:17 +0200

    setup.py: Add python 3.9 to supported Python version flags

--- setup.py
+++ setup.py
@@ -33,6 +33,7 @@ setup(
         'Development Status :: 3 - Alpha',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'License :: OSI Approved :: Apache Software License',
         'Topic :: Home Automation',
         'Framework :: AsyncIO',
