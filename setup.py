b8ee9f1beecaa1b3c7b01d044b4f8f91e0eaff52
Michael Thies <mail@mhthies.de>
2021-09-26 14:03:22 +0200

    Update asyncio-mqtt version requirement to 0.10.0 to fix typing

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
         'knx': ['knxdclient~=0.4.0'],
         'dmx': ['pyserial-asyncio>=0.3,<=0.5'],
         'midi': ['mido>=1.2.9,<2', 'python-rtmidi>=1.4.6,<2'],
-        'mqtt': ['paho-mqtt>=1.5.1,<2', 'asyncio-mqtt>=0.8.0,<0.11'],
+        'mqtt': ['paho-mqtt>=1.5.1,<2', 'asyncio-mqtt>=0.10.0,<0.11'],
     },
     classifiers=[
         'Development Status :: 3 - Alpha',
