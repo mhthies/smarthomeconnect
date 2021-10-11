9037998b9120d8f1c1563a5cbf9652618824d8f6
Michael Thies <mail@mhthies.de>
2021-10-11 20:28:52 +0200

    pulse: Add first real Pulseaudio test

--- test/interfaces/test_pulse.py
+++ test/interfaces/test_pulse.py
@@ -1,8 +1,17 @@
+import asyncio
+import os
+import shutil
+import subprocess
+import tempfile
+import time
 import unittest
 import ctypes
 import ctypes.util
+from pathlib import Path
+from typing import Tuple
 
 from shc.datatypes import RangeFloat1, Balance
+from .._helper import async_test, InterfaceThreadRunner, ExampleWritable
 
 libpulse_available = False
 try:
@@ -52,3 +61,117 @@ class PulseVolumeTests(unittest.TestCase):
         self.assertEqual(vol.map, vol2.map)
         for v1, v2 in zip(vol.values, vol3.values):
             self.assertAlmostEqual(v1 * 1.5, v2, places=4)
+
+
+@unittest.skipUnless(libpulse_available, "libpulse is not availabe on this system")
+@unittest.skipIf(shutil.which("pulseaudio") is None, "pulseaudio executable is not available in PATH")
+@unittest.skipIf(shutil.which("pactl") is None, "pactl executable is not available in PATH")
+class SinkConnectorTests(unittest.TestCase):
+    def setUp(self) -> None:
+        pulse_dir, self.pulse_process = create_dummy_instance()
+        time.sleep(0.05)  # let Pulseaudio start
+        self.pulse_url = f"unix:{pulse_dir / 'pulse' / 'native'}"
+        self.interface_runner = InterfaceThreadRunner(shc.interfaces.pulse.PulseAudioInterface,
+                                                      pulse_server_socket=self.pulse_url)
+        self.interface = self.interface_runner.interface
+
+    def tearDown(self) -> None:
+            self.interface_runner.stop()
+            self.pulse_process.terminate()
+            self.pulse_process.wait()
+
+    @async_test
+    async def test_sink_volume(self) -> None:
+        volume_connector1 = self.interface.sink_volume("testsink1")
+        target1 = ExampleWritable(shc.interfaces.pulse.PulseVolumeRaw).connect(volume_connector1)
+        volume_connector2 = self.interface.sink_volume("testsink2")
+        target2 = ExampleWritable(shc.interfaces.pulse.PulseVolumeRaw).connect(volume_connector2)
+        volume_connector3 = self.interface.sink_volume("testsink3")
+        target3 = ExampleWritable(shc.interfaces.pulse.PulseVolumeRaw).connect(volume_connector3)
+
+        # Initialize sink volumes
+        await self._run_pactl('set-sink-volume', 'testsink1', '90%')
+        await self._run_pactl('set-sink-volume', 'testsink2', '100%')
+
+        self.interface_runner.start()
+
+        # Read sink volumes
+        value1 = await asyncio.wrap_future(asyncio.run_coroutine_threadsafe(volume_connector1.read(),
+                                                                            loop=self.interface_runner.loop))
+        self.assertAlmostEqual(0.9, value1.values[0], places=3)
+        value2 = await asyncio.wrap_future(asyncio.run_coroutine_threadsafe(volume_connector2.read(),
+                                                                            loop=self.interface_runner.loop))
+        self.assertAlmostEqual(1.0, value2.values[0], places=3)
+        with self.assertRaises(shc.base.UninitializedError):
+            await asyncio.wrap_future(asyncio.run_coroutine_threadsafe(volume_connector3.read(),
+                                                                       loop=self.interface_runner.loop))
+
+        # External volume change to testsink2
+        target1._write.reset_mock()
+        target2._write.reset_mock()
+        await self._run_pactl('set-sink-volume', 'testsink2', '90%', '80%', '80%', '70%', '90%', '90%')
+        await asyncio.sleep(0.05)
+        target2._write.assert_called_once()
+        for v1, v2 in zip([0.9, 0.8, 0.8, 0.7, 0.9, 0.9], target2._write.call_args[0][0].values):
+            self.assertAlmostEqual(v1, v2, places=4)
+        target1._write.assert_not_called()
+        target3._write.assert_not_called()
+
+        # testsink3 added
+        output = await self._run_pactl('load-module', 'module-null-sink', 'sink_name=testsink3')
+        module_id = int(output)
+        await asyncio.sleep(0.05)
+        target3._write.assert_called_once()
+        for v1, v2 in zip([1.0, 1.0], target3._write.call_args[0][0].values):
+            self.assertAlmostEqual(v1, v2, places=4)
+
+        # unload testsink3
+        await self._run_pactl('unload-module', str(module_id))
+        await asyncio.sleep(0.05)
+        with self.assertRaises(shc.base.UninitializedError):
+            await asyncio.wrap_future(asyncio.run_coroutine_threadsafe(volume_connector3.read(),
+                                                                       loop=self.interface_runner.loop))
+
+        # Write to testsink1's volume
+        value1_new = value1._replace(values=[0.8, 0.7])
+        await asyncio.wrap_future(asyncio.run_coroutine_threadsafe(volume_connector1.write(value1_new, [self]),
+                                                                   loop=self.interface_runner.loop))
+        response = await self._run_pactl('get-sink-volume', 'testsink1')
+        self.assertIn("front-left: 52429", response)
+        self.assertIn("front-right: 45875", response)
+
+    async def _run_pactl(self, *args) -> str:
+        proc = await asyncio.create_subprocess_exec(
+            'pactl',
+            '-s',
+            self.pulse_url,
+            *args,
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE,
+        )
+        stdout, stderr = await proc.communicate()
+        if proc.returncode:
+            raise RuntimeError(f"pactl failed with exit code {proc.returncode}. StdErr:\n{stderr.decode()}")
+        return stdout.decode()
+
+
+def create_dummy_instance() -> Tuple[Path, subprocess.Popen]:
+    pulse_dir = tempfile.mkdtemp(prefix='shc-pulse-tests.')
+    env = dict(PATH=os.environ['PATH'], XDG_RUNTIME_DIR=pulse_dir, PULSE_STATE_PATH=pulse_dir)
+    proc = subprocess.Popen(
+        ['pulseaudio', '--daemonize=no', '--fail',
+         '-nF', '/dev/stdin', '--exit-idle-time=-1', '--log-level=error'],
+        env=env, stdin=subprocess.PIPE)
+    proc.stdin.write("""
+load-module module-suspend-on-idle
+load-module module-filter-heuristics
+load-module module-filter-apply
+load-module module-switch-on-port-available
+load-module module-native-protocol-unix
+load-module module-null-sink sink_name=testsink1
+load-module module-null-sink sink_name=testsink2 channels=6 channel_map=front-left,front-right,rear-left,rear-right,front-center,lfe
+load-module module-null-source source_name=testsource1
+load-module module-null-source source_name=testsource2
+""".encode())
+    proc.stdin.close()
+    return Path(pulse_dir), proc
