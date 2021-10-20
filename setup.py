02819af41c1ed92fcaadaedf1c2a72445dec8109
Michael Thies <mail@mhthies.de>
2021-10-20 13:21:56 +0200

    Bump version to 0.5.0

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md'), encoding='utf-8'
 
 setup(
     name='smarthomeconnect',
-    version='0.4.0',
+    version='0.5.0',
     description='The Smart Home Connect home automation framework based on AsyncIO',
     long_description=long_description,
     long_description_content_type='text/markdown',
