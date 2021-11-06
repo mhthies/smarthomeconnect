a8401ed15b3cf85a0d592441962acfac83cca4f4
Michael Thies <mail@mhthies.de>
2021-11-06 16:22:12 +0100

    web_ui_src: Split shc_base.js

--- web_ui_src/widgets/basic_widgets.js
+++ web_ui_src/widgets/basic_widgets.js
@@ -10,29 +10,7 @@
  * specific language governing permissions and limitations under the License.
  */
 
-import iro from '@jaames/iro';
 import $ from 'jquery';
-
-function ws_path(path) {
-    if (path.startsWith("/")) {
-        const loc = window.location;
-        let new_proto;
-        if (loc.protocol === "https:") {
-            new_proto = "wss:";
-        } else {
-            new_proto = "ws:";
-        }
-        return new_proto + "//" + loc.host + path;
-    } else {
-        const loc = new URL(path);
-        if (loc.protocol === "https:") {
-            loc.protocol = "wss:";
-        } else {
-            loc.protocol = "ws:";
-        }
-        return loc.href;
-    }
-}
 
 function forEachNodeRecursive(node, func) {
     /**
@@ -240,22 +218,6 @@ function SliderWidget(domElement, writeValue) {
     }
 }
 
-function ColorChoserWidget(domElement, writeValue) {
-    const id = parseInt(domElement.getAttribute('data-id'));
-    this.subscribeIds = [id];
-    var colorPicker = new iro.ColorPicker(domElement, {
-        color: "#000"
-    });
-
-    colorPicker.on('input:end', function(color) {
-        writeValue(id, [color.red, color.green, color.blue]);
-    });
-
-    this.update = function(value, for_id) {
-        colorPicker.color.rgb = {'r': value[0], 'g': value[1], 'b': value[2]};
-    };
-}
-
 function HideRowWidget(domElement, _writeValue) {
     const id = parseInt(domElement.getAttribute('data-id'));
     this.subscribeIds = [id];
@@ -279,122 +241,4 @@ WIDGET_TYPES.set('button', ButtonWidget);
 WIDGET_TYPES.set('text-display', TextDisplayWidget);
 WIDGET_TYPES.set('text-input', TextInputWidget);
 WIDGET_TYPES.set('slider', SliderWidget);
-WIDGET_TYPES.set('colorchoser', ColorChoserWidget);
 WIDGET_TYPES.set('hiderow', HideRowWidget);
-
-(function () {
-    let widgetMap = new Map();
-    let ws;
-    let connectionErrorToast = null;  // The Semantic UI 'toast' element which indicates a lost websocket connection.
-    let connectionPreviouslyEstablished = false; // true, once we established a websocket connection successfully.
-
-    function init() {
-        console.info("Intializing Smart Home Connect web UI.");
-        const widgetElements = document.querySelectorAll('[data-widget]');
-        for (const widgetElement of widgetElements) {
-            let type = widgetElement.getAttribute('data-widget');
-            let obj = new (WIDGET_TYPES.get(type))(widgetElement, writeValue);
-            for (const the_id of obj.subscribeIds) {
-                widgetMap.set(the_id, obj);
-            }
-        }
-
-        openWebsocket();
-    }
-
-    function openWebsocket() {
-        console.info("Opening websocket ...");
-        ws = new WebSocket(ws_path(shcRootURL + '/ws'));
-        ws.onopen = subscribe;
-        ws.onmessage = dispatch_message;
-        ws.onclose = function (e) {
-            console.info('Socket is closed. Reconnect will be attempted in 1 second.', e.reason);
-            if (!connectionErrorToast) {
-                connectionErrorToast = $('body').toast({
-                    'message': connectionPreviouslyEstablished
-                        ? "Connection to websocket server failed ..."
-                        : "Connection to SHC server lost ...",
-                    'class': 'error',
-                    showIcon: 'exclamation circle',
-                    displayTime: 0,
-                    closeOnClick: false
-                });
-            }
-            setTimeout(function () {
-                openWebsocket();
-            }, 1000);
-        };
-        ws.onerror = function (err) {
-            console.error('Socket encountered error: ', err.message, 'Closing socket');
-            ws.close();
-        };
-    }
-
-    function subscribe() {
-        if (connectionErrorToast) {
-            connectionErrorToast.toast('close');
-            connectionErrorToast = null;
-        }
-        console.info("Websocket opened. Subscribing widget values ...");
-        // Send serverToken to server to reload page on server restarts
-        ws.send(JSON.stringify({
-            'serverToken': shcServerToken
-        }));
-        // Subscribe to WebConnector objects
-        for (const id of widgetMap.keys()) {
-            ws.send(JSON.stringify({
-                'sub': true,
-                'id': id
-            }));
-        }
-    }
-
-    function dispatch_message(messageEvent) {
-        console.debug("Received message " + messageEvent.data);
-        let data = JSON.parse(messageEvent.data);
-        if (data['reload']) {
-            console.debug("Reloading page, because server told us to do so.");
-            location.reload();
-        } else if (data['id']) {
-            console.debug("Updating widget id " + data['id'].toString() + " ...");
-            widgetMap.get(data['id']).update(data['v'], data['id']);
-        } else {
-            console.error("Don't know how to handle websocket message ", data);
-        }
-    }
-
-    function writeValue(id, value) {
-        console.debug("Writing new value " + JSON.stringify(value) + " for widget id " + id.toString());
-        try {
-            if (!ws)
-                throw "Websocket not available";
-            if (ws.readyState !== WebSocket.OPEN)
-                throw "Websocket is not open."
-            ws.send(JSON.stringify({
-                'id': id,
-                'v': value
-            }));
-        } catch (e) {
-            console.error("Error while sending value via websocket:", e);
-            $('body').toast({
-                'message': "Error while sending value.",
-                'class': 'warning',
-                showIcon: 'exclamation circle'});
-        }
-    }
-
-    document.addEventListener('DOMContentLoaded', (event) => init());
-})();
-
-$(function() {
-    $('.main-menu .ui.dropdown').dropdown();
-    $('.ui.sidebar').sidebar({transition: 'overlay'}).sidebar('attach events', '#mobile_item');
-    $('.shc.image-container .with-popup>*').each(function(){
-        $(this).popup({
-            on: 'click',
-            popup: $(this).parent().attr('data-popup-id'),
-            position: $(this).parent().attr('data-popup-position'),
-            movePopup: false,
-            forcePosition: true});
-    });
-});
