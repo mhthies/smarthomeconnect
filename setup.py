b1dd7cac6ffac3dc9e168879e822f16e3377384d
Michael Thies <mail@mhthies.de>
2022-04-16 15:17:37 +0200

    Use setup.cfg for static packaging metadata

--- setup.py
+++ setup.py
@@ -10,9 +10,8 @@
 # "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 # specific language governing permissions and limitations under the License.
 
-import os.path
 import subprocess
-from setuptools import setup, find_packages
+from setuptools import setup
 from setuptools.command.build_py import build_py
 
 
@@ -23,49 +22,8 @@ class custom_build_py(build_py):
         super().run()
 
 
-with open(os.path.join(os.path.dirname(__file__), 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
-
 setup(
-    name='smarthomeconnect',
-    version='0.6.0',
-    description='The Smart Home Connect home automation framework based on AsyncIO',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    author='Michael Thies',
-    author_email='mail@mhthies.de',
-    url='https://github.com/mhthies/smarthomeconnect',
     cmdclass={
         'build_py': custom_build_py
     },
-    packages=find_packages(exclude=["test", "test.*"]),
-    include_package_data=True,
-    package_data={
-        "shc.web": ["static/pack/*"]
-    },
-    python_requires='~=3.7',
-    install_requires=[
-        'aiohttp>=3.6,<4',
-        'jinja2>=2.11,<4',
-        'MarkupSafe>=1.1,<3',
-    ],
-    extras_require={
-        'mysql': ['aiomysql>=0.0.21,<=0.0.22'],
-        'knx': ['knxdclient~=0.4.0'],
-        'dmx': ['pyserial-asyncio>=0.3,<0.7'],
-        'midi': ['mido>=1.2.9,<2', 'python-rtmidi>=1.4.6,<2'],
-        'mqtt': ['paho-mqtt>=1.5.1,<2', 'asyncio-mqtt>=0.10.0,<0.13'],
-        'pulse': ['pulsectl_asyncio~=0.2.0'],
-        'telegram': ['aiogram~=2.18'],
-    },
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'License :: OSI Approved :: Apache Software License',
-        'Topic :: Home Automation',
-        'Framework :: AsyncIO',
-    ],
 )
