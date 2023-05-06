0b6813bcd56ba6d6466ad8afcd1ebabfe4c6b922
Michael Thies <mail@mhthies.de>
2023-05-06 14:49:17 +0200

    Setup flake8 for codestyle checks and mask flase-positives

--- test/test_examples.py
+++ test/test_examples.py
@@ -13,41 +13,41 @@ async def shutdown() -> None:
 class BasicTest(unittest.TestCase):
     def test_ui_showcase(self) -> None:
         shc.supervisor.event_loop.create_task(shutdown())
-        import example.ui_showcase  # type: ignore
+        import example.ui_showcase  # type: ignore  # noqa: F401
         shc.main()
 
     def test_ui_logging_showcase(self) -> None:
         shc.supervisor.event_loop.create_task(shutdown())
 
-        import example.ui_logging_showcase  # type: ignore
+        import example.ui_logging_showcase  # type: ignore  # noqa: F401
         shc.main()
 
     def test_server_client_example(self) -> None:
         shc.supervisor.event_loop.create_task(shutdown())
 
         # The examples should actually be able to coexist in a single SHC instance
-        import example.server_client.server  # type: ignore
-        import example.server_client.client  # type: ignore
+        import example.server_client.server  # type: ignore  # noqa: F401
+        import example.server_client.client  # type: ignore  # noqa: F401
         shc.main()
 
     def test_tasmota_led_example(self) -> None:
-        import example.tasmota_led_ir_with_ui  # type: ignore
+        import example.tasmota_led_ir_with_ui  # type: ignore  # noqa: F401
 
     def test_telegram_example(self) -> None:
-        import example.telegram  # type: ignore
+        import example.telegram  # type: ignore  # noqa: F401
 
     def test_pulseaudio_sink_example(self) -> None:
-        import example.pulseaudio_sink  # type: ignore
+        import example.pulseaudio_sink  # type: ignore  # noqa: F401
 
     def test_knx_specifics_example(self) -> None:
-        import example.knx_specifics  # type: ignore
+        import example.knx_specifics  # type: ignore  # noqa: F401
 
     def test_custom_ui_widet_example(self) -> None:
-        import example.custom_ui_widget.main  # type: ignore
+        import example.custom_ui_widget.main  # type: ignore  # noqa: F401
         # TODO add selenium test for actual position of the indicator
 
     def test_sun_position_weather_forecast_example(self) -> None:
-        import example.sun_position_weather_forecast  # type: ignore
+        import example.sun_position_weather_forecast  # type: ignore  # noqa: F401
 
     @classmethod
     def setUpClass(cls) -> None:
